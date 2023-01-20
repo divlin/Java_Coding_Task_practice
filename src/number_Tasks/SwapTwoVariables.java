c8b075e6b4724f2e4fe23b64e8b2deab34b3941a
divlin <divlin55@hotmail.com>
2023-01-19 21:55:05 -0500

    Number added swap two variables class

---
+++ src/number_Tasks/SwapTwoVariables.java
@@ -0,0 +1,23 @@
+package number_Tasks;
+
+public class SwapTwoVariables {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        a = a+b; // a= 30
+        b = a-b; // b = 10
+        a = a-b; // a = 20;
+
+        System.out.println(a);  //20
+        System.out.println(b);  //10
+
+
+    }
+}
+
+/*
+    Swap two variable' values without using a third variable
+     */
