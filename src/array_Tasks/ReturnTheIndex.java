77eba81d59adee5717933269041ec492be64559d
divlin <divlin55@hotmail.com>
2023-01-20 20:45:42 -0500

    Array return the index class updated

--- src/array_Tasks/ReturnTheIndex.java
+++ src/array_Tasks/ReturnTheIndex.java
@@ -9,16 +9,20 @@ public class ReturnTheIndex {
 
         int[] numbers = {1 , 2, 100, 7, 11, 15, 999};
 
+
         int target = 9;
 
         String s = "";
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length; j++) {
-                if (numbers[i]+numbers[j]==9){
+            for (int j = i+1; j < numbers.length; j++) {
+                if (numbers[i]+numbers[j]==target){
                     s+= "" + i + "" + j;
                 }
             }
         }
+
+
+
         System.out.println(Arrays.toString(s.split("")));
 
 
@@ -30,7 +34,6 @@ public class ReturnTheIndex {
 input:numbers = {1 , 2, 100, 7, 11, 15, 999 };
  target = 9
  output: [1, 3]
-find the sum of 2 integer values equal
-to 9 and return there indexes
+find the sum of 2 integer values equal to 9 and return there indexes
 
  */
